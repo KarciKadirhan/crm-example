78370770dfdaaf666ead3fb1bc8f766599aef60f
kadir <kadir@KADIRHAN>
2022-10-28 11:40:01 +0300

    Add project files.

---
+++ CRMExample.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRMExample.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
