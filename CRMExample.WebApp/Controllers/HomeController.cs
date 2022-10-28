28a3ddff604f714a84714931d16f0989dd568520
Kadirhan Karcı <kadirhankarci@gmail.com>
2022-10-28 12:42:01 +0300

    Template proje içine aktarıldı. Dashboard sayfası oluşturuldu.

--- CRMExample.WebApp/Controllers/HomeController.cs
+++ CRMExample.WebApp/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace CRMExample.WebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Dashboard()
         {
             return View();
         }
